889e356823233f936f246ed2922e77508323f21e
Krae <kraewind@gmail.com>
2021-05-21 16:09:55 -0400

    commit tree adbfff539403f90fcd0ae597becef1fe7d284530

---
+++ index.js
@@ -0,0 +1,24 @@
+function Scooter(year, color, model) {
+    return {
+    year,
+    color,
+    model,
+  }
+}
+
+function Driver(name, age, experience) {
+    return {
+    name,
+    age,
+    experience,
+  }
+}
+
+function PickUpLocation(address, city) {
+    return {
+    address,
+    city,
+  }
+}
+
+scooty = new Scooter(2002, 'red', 'arrow')
